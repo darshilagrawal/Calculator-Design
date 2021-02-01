272d15066709d07dea0740e628d850d48192ef07
darshilagrawal <darshilagrawal2000@gmail.com>
2021-02-01 11:17:11 +0530

    Comments and Identation

--- Calculator-Design/ViewController.swift
+++ Calculator-Design/ViewController.swift
@@ -1,10 +1,3 @@
-//
-//  ViewController.swift
-//  Calculator Layout iOS13
-//
-//  Created by Angela Yu on 01/07/2019.
-//  Copyright © 2019 The App Brewery. All rights reserved.
-//
 
 import UIKit
 
